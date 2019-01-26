6139cfc7c977d9e295dad32720d4f98ce93e66f2
TheKit <nekit1000@gmail.com>
2019-01-26 05:29:17 +0300

    render/egl: fallback to plain eglGetDisplay

--- render/egl.c
+++ render/egl.c
@@ -123,8 +123,10 @@ bool wlr_egl_init(struct wlr_egl *egl, EGLenum platform, void *remote_display,
 	if (platform == EGL_PLATFORM_SURFACELESS_MESA) {
 		assert(remote_display == NULL);
 		egl->display = eglGetPlatformDisplayEXT(platform, EGL_DEFAULT_DISPLAY, NULL);
-	} else {
+	} else if (platform) {
 		egl->display = eglGetPlatformDisplayEXT(platform, remote_display, NULL);
+	} else {
+		egl->display = eglGetDisplay(remote_display);
 	}
 	if (egl->display == EGL_NO_DISPLAY) {
 		wlr_log(WLR_ERROR, "Failed to create EGL display");
